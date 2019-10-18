1e9ecd98b38cc33827167cb880bf939d4a371c2e
gaaithri <gaaithrinathan@gmail.com>
2019-10-18 12:33:37 +0530

    bootstrap-ract added

---
+++ sparkjoy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Landingpage from './Components/Landingpage';
+
+function App() {
+  return (
+    <div className="App">
+    
+        
+    </div>
+  );
+}
+
+export default App;
