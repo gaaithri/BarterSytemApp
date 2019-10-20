299dc410f8bacc56c3aaae1831956eefc309c5ec
gaaithri <gaaithrinathan@gmail.com>
2019-10-20 15:18:54 +0530

    Fb update

--- sparkjoy/src/components/page/Donationdetails.js
+++ sparkjoy/src/components/page/Donationdetails.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Donationdetails = ({item}) => {
     return(
         <div className="card itemdetails">
-                <div  className="image"><img src= {item.image}></img>
+                <div  className="image"><img src= {item.image}  width="250px" height="200px"></img>
                     <div className="card-content ">
                       <span className="card-title"> {item.name}</span>
-                      <p>Donated by:{item.dname}</p>
-                       {/* <p> Donated By Rani </p> */}
-                      {/* <p className="text"> 3 October </p>    */}
+                       <p> Donated By {item.dname} </p>
+                      <p className="text"> 3 September </p>   
+                      <br />
                    </div>                      
                 </div>
             </div>
